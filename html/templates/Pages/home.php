66253d5e7755cffeac0086e91fdcc13ab1c9445b
devil-works <tkhr6022@gmail.com>
2022-06-29 03:37:44 +0900

    Styled the code area

--- html/templates/Pages/home.php
+++ html/templates/Pages/home.php
@@ -8,19 +8,19 @@
         </div>
         <div class="swiper-slide" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg')">
             <div class="text-wrapper ls-1">
-            <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築2</p>
+                <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築2</p>
                 <p class="blog-subTtl small">サブタイトルサブタイトルサブタイトルサブタイトル</p>
             </div>
         </div>
         <div class="swiper-slide" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg')">
             <div class="text-wrapper ls-1">
-            <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築3</p>
+                <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築3</p>
                 <p class="blog-subTtl small">サブタイトルサブタイトルサブタイトルサブタイトル</p>
             </div>
         </div>
         <div class="swiper-slide" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg')">
             <div class="text-wrapper ls-1">
-            <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築4</p>
+                <p class="blog-ttl my-4 h2">CakePHPでブログサイトの構築4</p>
                 <p class="blog-subTtl small">サブタイトルサブタイトルサブタイトルサブタイトルサブタイトル</p>
             </div>
         </div>
@@ -29,4 +29,39 @@
     <div class="swiper-button-prev"></div>
     <div class="swiper-pagination"></div>
 </div>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row my-5">
+            <h2>Latest code</h2>
+        </div>
+        <div class="row">
+            <a href="foo" class="col-md-4">
+                <div class="code-box card mb-4 shadow-sm">
+                    <h6 class="code-ttl text-center w-100">fooooo</h6>
+                    <small class="lang-type">PHP</small>
+                    <div class="code-body">
+                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                    </div>
+                </div>
+            </a>
+            <a href="foo" class="col-md-4">
+                <div class="code-box card mb-4 shadow-sm">
+                    <h6 class="code-ttl text-center w-100">fooooo</h6>
+                    <small class="lang-type">PHP</small>
+                    <div class="code-body">
+                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                    </div>
+                </div>
+            </a>
+            <a href="foo" class="col-md-4">
+                <div class="code-box card mb-4 shadow-sm">
+                    <h6 class="code-ttl text-center w-100">fooooo</h6>
+                    <small class="lang-type">PHP</small>
+                    <div class="code-body">
+                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
 </div>
--- html/templates/layout/bk/default.php
+++ html/templates/layout/bk/default.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -19,31 +18,27 @@ $cakeDescription = 'devil code: Web development in Sendai, Japan';
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-  <?= $this->Html->charset() ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>
-    <?= $cakeDescription ?>:
-    <?= $this->fetch('title') ?>
-  </title>
-  <?= $this->Html->meta('icon') ?>
-
-  <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-  <?= $this->Html->css(['bootstrap.min', 'style', 'sunburst']) ?>
-  <?= $this->fetch('meta') ?>
-  <?= $this->fetch('css') ?>
-  <?= $this->fetch('script') ?>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+    <?= $this->Html->css(['bootstrap.min']) ?>
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
 </head>
-
 <body>
-  <header>
+<header class="fixed-top">
     <div class="px-4 py-3 bg-dark text-white">
       <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
             <img src="img/brand-logo_white.png" height="32">
-            <!-- <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> -->
+          <!-- <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> -->
           </a>
 
           <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
@@ -67,20 +62,21 @@ $cakeDescription = 'devil code: Web development in Sendai, Japan';
       </div>
     </div>
   </header>
-  <main class="main">
-    <?= $this->fetch('content') ?>
-  </main>
-  <footer class="py-3 my-4 bg-dark text-white">
+    <main class="main">
+        <div class="container">
+            <?= $this->Flash->render() ?>
+            <?= $this->fetch('content') ?>
+        </div>
+    </main>
+    <footer class="py-3 my-4 bg-dark text-white">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Blog</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">GitHub</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">Blog</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">GitHub</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">Contact</a></li>
     </ul>
-    <p class="text-center text-muted">© 2022 devil code</p>
+    <p class="text-center">© 2021 devil code</p>
   </footer>
-  <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-  <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-  <?= $this->Html->script(['index','bootstrap.bundle.min']) ?>
+    <?= $this->Html->script(['index']) ?>
+    <?= $this->Html->script(['bootstrap.bundle.min']) ?>
 </body>
-
 </html>
