694530568b16f41a531929006c4b7f3ab8140178
devil-works <tkhr6022@gmail.com>
2022-07-03 23:35:48 +0900

    Created the blog area

--- html/templates/Pages/home.php
+++ html/templates/Pages/home.php
@@ -31,41 +31,134 @@
     <div class="swiper-button-prev"></div>
     <div class="swiper-pagination"></div>
 </div>
-<div class="album py-5 bg-light">
-    <div class="container">
-        <div class="row my-5">
-            <h2>Latest code</h2>
-        </div>
-        <div class="row">
-            <a href="foo" class="col-md-4">
-                <div class="code-box card mb-4 shadow-sm">
-                    <h6 class="code-ttl text-center w-100">fooooo</h6>
-                    <small class="lang-type">PHP</small>
-                    <div class="code-body">
-                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+<section>
+
+    <div class="album py-5 bg-white">
+        <div class="container">
+            <div class="row my-5">
+                <h2>Latest code</h2>
+            </div>
+            <div class="row">
+                <a href="foo" class="col-md-4">
+                    <div class="code-box card mb-4 shadow-sm">
+                        <h6 class="code-ttl text-center w-100">fooooo</h6>
+                        <small class="lang-type">PHP</small>
+                        <div class="code-body">
+                            <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="foo" class="col-md-4">
-                <div class="code-box card mb-4 shadow-sm">
-                    <h6 class="code-ttl text-center w-100">fooooo</h6>
-                    <small class="lang-type">PHP</small>
-                    <div class="code-body">
-                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                </a>
+                <a href="foo" class="col-md-4">
+                    <div class="code-box card mb-4 shadow-sm">
+                        <h6 class="code-ttl text-center w-100">fooooo</h6>
+                        <small class="lang-type">PHP</small>
+                        <div class="code-body">
+                            <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="foo" class="col-md-4">
-                <div class="code-box card mb-4 shadow-sm">
-                    <h6 class="code-ttl text-center w-100">fooooo</h6>
-                    <small class="lang-type">PHP</small>
-                    <div class="code-body">
-                        <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                </a>
+                <a href="foo" class="col-md-4">
+                    <div class="code-box card mb-4 shadow-sm">
+                        <h6 class="code-ttl text-center w-100">fooooo</h6>
+                        <small class="lang-type">PHP</small>
+                        <div class="code-body">
+                            <pre class="code-text prettyprint lang-php">console.log('Hello World')</pre>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
-</div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row my-5">
+            <h2>Latest posts</h2>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-primary">World</strong>
+                        <h3 class="mb-0">Featured post</h3>
+                        <div class="mb-1 text-muted">Nov 12</div>
+                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="stretched-link">Continue reading</a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-success">Design</strong>
+                        <h3 class="mb-0">Post title</h3>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="stretched-link">Continue reading</a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section>
+    <div class="container">
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-primary">World</strong>
+                        <h3 class="mb-0">Featured post</h3>
+                        <div class="mb-1 text-muted">Nov 12</div>
+                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="stretched-link">Continue reading</a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-success">Design</strong>
+                        <h3 class="mb-0">Post title</h3>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="stretched-link">Continue reading</a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 <?= $this->Html->script('https://unpkg.com/swiper@8/swiper-bundle.min.js', ['block' => 'scriptBottom']) ?>
