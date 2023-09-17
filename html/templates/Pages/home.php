61dc9ba6d61a022ae0de4f13b5e3cd236f64c7f5
devil-works <tkhr6022@gmail.com>
2023-09-17 20:52:41 +0900

    Test

--- html/templates/Pages/home.php
+++ html/templates/Pages/home.php
@@ -11,6 +11,7 @@
 
 
 
+
 <?= $this->Html->css(['swiper-bundle.min.css'], ['block' => 'css']) ?>
 <?= $this->Html->meta(["name"=>"description","content"=>"devil code(デビルコード)ではプログラミングの技術メモや学習記録、ITのキャリアプランや就職などについてブログを投稿しています。"],null,["block"=>'meta']); ?>
 <?= $this->Html->meta(["property"=>"og:title","content"=>"【devil code】デビルコード プログラミングと情報技術"],null,["block"=>'meta']); ?>
