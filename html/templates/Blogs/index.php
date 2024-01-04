51d5d49889c25b196699bf780860cd5de835109a
devil-works <tkhr6022@gmail.com>
2024-01-05 00:09:32 +0900

    Add list of article card

--- html/templates/Blogs/index.php
+++ html/templates/Blogs/index.php
@@ -45,110 +45,141 @@ $this->assign('canonical', '<link rel="canonical" href="https://devil-code.com/b
 <?= $this->Html->meta(["property"=>"og:site_name","content"=>"devil code"],null,["block"=>'meta']); ?>
 <?= $this->Html->meta(["property"=>"og:locale","content"=>"ja_JP"],null,["block"=>'meta']); ?>
 
-<div class="swiper mySwiper">
-    <div class="swiper-wrapper">
-        <?php foreach ($tops as $index => $top) : ?>
-            <div class="swiper-slide slide-mask " style="background-image: url('../files/blogs/thumbnails/<?= sprintf("%010d", $top->id) ?>.webp')">
-                <div class="text-wrapper ls-1">
-                    <h2 class="blog-ttl px-3 py-1 mx-3 mt-4 d-inline-block" style="margin-bottom: 120px;"><?= h($top->title) ?></h2>
-                    <div class="text-wrapper ls-1 mt-5 pt-5">
-                        <a href="/blogs/<?= h($top->blogs_category->category_label) ?>/<?= h($top->slug) ?>/" class="border-btn px-4 px-md-5 py-3 border border-3 border-white text-white text-nowrap">ブログを見る</a>
-                    </div>
-                </div>
-            </div>
-            <?= $this->Html->meta(["property"=>"og:image","content"=>"https://devil-code.com/files/blogs/thumbnails/<?= sprintf('%010d', $top->id) ?>.webp"],null,["block"=>'meta']); ?>
-        <?php endforeach; ?>
-    </div>
-</div>
-
-<section class="blog ls-1">
-    <div class="album bg-white pb-5">
-        <div class="container">
-            <div class="row mb-5 align">
-                <h2>最近の記事</h2>
-                <p>Lately posts</p>
-            </div>
-            <div class="row mb-2 px-3">
-                <div class="col-xl-8 card-item pt-3 pt-md-0">
-                    <ul class="list-group list-group-flush">
-                        <?php foreach ($news as $index => $new) : ?>
-                            <li class="list-group-item mb-3 mb-md-0 pt-md-4">
-                                <a href="/blogs/<?= h($new->blogs_category->category_label) ?>/<?= h($new->slug) ?>/">
-                                    <div class="d-md-flex"  style="line-height: 30px;">
-                                        <div class="d-flex">
-                                            <span class="catTag me-3" style="background-color:<?= h($new->blogs_category->category_color) ?>;"><?= h($new->blogs_category->category_label) ?></span>
-                                            <span class="me-3 text-muted" style="font-size: 12px;"><?= h($new->created->i18nFormat('yyyy.MM.dd')) ?></span>
-                                        </div>
-                                        <p class="mt-3 mt-md-0 flex-wrap-reverse"><?= h($new->title) ?></p>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            </div>
+<section class="p-blogs__blogs">
+    <div class="l-container l-container__common">
+        <div class="p-blogs__sectionTitle">
+           <h3 class="c-headingSize__2xl">Lately</h3>
+           <span>最近の記事</span>
         </div>
-    </div>
-    <div class="container py-5">
-        <div class="row my-5">
-            <h2>人気記事</h2>
-            <p>Featured</p>
-        </div>
-        <div class="row mb-2 px-3 d-flex justify-content-between align">
-            <?php foreach ($features as $index => $feature) : ?>
-                <a href="/blogs/<?= h($feature->blogs_category->category_label) ?>/<?= h($feature->slug) ?>/" class="col-md-12 col-lg-4 mb-5 pe-lg-5 h-100">
-                    <div class="row card-item overflow-hidden position-relative">
-                        <div class="col-auto blog-img w-100" style="overflow:hidden; background-image:url('https://devil-code.com/files/blogs/thumbnails/<?= sprintf("%010d", $feature->id) ?>.webp'); background-position: center; background-size: contain; background-repeat: no-repeat; height: 180px;">
-                        </div>
-                        <span class="mb-2 catTag position-absolute" style="top: 32px; right: 16px; background-color:<?= h($feature->blogs_category->category_color) ?>;"><?= h($feature->blogs_category->category_label) ?></span>
 
-                        <div class="col pb-4 px-4 d-flex flex-column position-relative" style="height: 180px;">
-                            <h3 class="mt-3 mb-0 pb-5 fs-5 blog-ttl lh-base"><?= h($feature->title) ?></h3>
-                            <div class="mt-3 w-100 text-muted d-flex justify-content-between align-items-center position-absolute" style="bottom: 24px;">
-                                <span style="font-size: 12px;"><?= h($feature->created->i18nFormat('yyyy.MM.dd')) ?></span>
-                                <span class="arrow"></span>
-                            </div>
-                        </div>
+        <div class="l-container__grid  l-container__grid-column3 p-blogs__articles">
+            <?php foreach ($news as $index => $new) : ?>
+                <article class="c-articleListCards">
+                    <img itemprop="image" src="/files/blogs/thumbnails/<?= sprintf("%010d", $new->id); ?>.webp" alt="ブログ <?= h($new->title); ?>のサムネイル" width="1200" height="630">
+                    <a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/" tabindex="-1" class="c-articleListCards__link"></a>
+                    <header class="c-articleListCards__header">
+                        <a class="c-articleListCards__categoryLink" href="/blogs/<?= h($new->blogs_category->category_name) ?>/" tabidnex="1">
+                            <div class="c-articleListCards__categoryName"><?= h($new->blogs_category->category_name) ?></div>
+                        </a>
+                        <span class="c-articleListCards__created"><time datetime="<?= $this->Time->format($new->created, 'yyyy-MM-dd\'T\'HH:mm:ssXXX'); ?>"><?= h($new->created->i18nFormat('yyyy-MM-dd')) ?></time></span>
+                    </header>
+                    <div class="c-articleListCards__titleContainer">
+                        <h4 class="c-articleListCards__title"><a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/"><?= h($new->title) ?></a></h4>
                     </div>
-                </a>
+                    <footer class="c-articleListCards__footer">
+                        <ul class="c-articleListCards__tags">
+                            <!-- テンプレート内のループ -->
+                            <?php if(isset($blogTags[$new->id])): ?>
+                                <?php foreach ($blogTags[$new->id] as $index => $blogTag) : ?>
+                                <li>
+                                    <a href="/blogs/<?= $blogTag["tag_slug"]; ?>/">
+                                        <?= $blogTag["tag_name"]; ?>
+                                    </a>
+                                </li>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                    </footer>
+                </article>
             <?php endforeach; ?>
-        </div>
+          </div>
     </div>
-    <div class="container">
-        <div class="row my-5 d-flex">
-            <div class="col-md-8 me-md-1">
-				<h2>記事一覧</h2>
-				<p class="mb-5">Posts</p>
-                <div class="d-flex justify-content-between align-items-center mb-5">
-                    <span class="articleTotalCount">全<?= h($totalCount) ?>件</span>
-                    <a href="/blogs/list/" class="view-all ms-auto me-0">もっと見る</a>
-                </div>
-                <?php foreach ($blogs as $index => $blog) : ?>
-                    <a href="/blogs/<?= h($blog->blogs_category->category_label) ?>/<?= h($blog->slug) ?>/" class="pe-lg-5 h-100">
-                        <div class="d-md-flex justify-content-between align-items-center">
-                            <h3 class="fs-5 fs-lg-4 blog-ttl lh-base"><?= h($blog->title) ?></h3>
-                            <span class="catTag" style="background-color:<?= h($blog->blogs_category->category_color) ?>;"><?= h($blog->blogs_category->category_label) ?></span>
-                        </div>
-                        <div class="mb-4 text-muted"><span style="font-size: 12px;"><?= h($blog->created->i18nFormat('yyyy.MM.dd')) ?></div>
-                        <p class="mb-5 text-muted text-break" style="font-weight:100;"><?= mb_substr(strip_tags($blog->body),0,120) ?></p>
-                    </a>
-                <?php endforeach; ?>
-            </div>
 
-            <div class="category_tagWrapper mt-5 col-md-3 h-100 px-md-5 sticky-top">
-                <h5>カテゴリー</h5>
-                <p>Category</p>
-                <ul class="categories ps-0 mt-3 mb-5">
-	                <?php foreach ($categories as $category) : ?>
-                        <li class="categoryLink d-inline-block">
-                            <a class="d-inline-block" href="/blogs/<?= h($category->cat_label) ?>/"><?= h($category->cat_label) ?></a>
-                        </li>
-	                <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
+    <div class="l-container c-viewMore">
+        <a class=" c-viewMore__link" href="/blogs">ブログをもっと見る</a>
     </div>
 </section>
+
+<section class="p-blogs__blogs">
+    <div class="l-container l-container__common">
+        <div class="p-blogs__sectionTitle">
+           <h3 class="c-headingSize__2xl">Computer Science</h3>
+           <span>コンピューターサイエンス</span>
+        </div>
+
+        <div class="l-container__grid  l-container__grid-column3 p-blogs__articles">
+            <?php foreach ($news as $index => $new) : ?>
+                <article class="c-articleListCards">
+                    <img itemprop="image" src="/files/blogs/thumbnails/<?= sprintf("%010d", $new->id); ?>.webp" alt="ブログ <?= h($new->title); ?>のサムネイル" width="1200" height="630">
+                    <a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/" tabindex="-1" class="c-articleListCards__link"></a>
+                    <header class="c-articleListCards__header">
+                        <a class="c-articleListCards__categoryLink" href="/blogs/<?= h($new->blogs_category->category_name) ?>/" tabidnex="1">
+                            <div class="c-articleListCards__categoryName"><?= h($new->blogs_category->category_name) ?></div>
+                        </a>
+                        <span class="c-articleListCards__created"><time datetime="<?= $this->Time->format($new->created, 'yyyy-MM-dd\'T\'HH:mm:ssXXX'); ?>"><?= h($new->created->i18nFormat('yyyy-MM-dd')) ?></time></span>
+                    </header>
+                    <div class="c-articleListCards__titleContainer">
+                        <h4 class="c-articleListCards__title"><a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/"><?= h($new->title) ?></a></h4>
+                    </div>
+                    <footer class="c-articleListCards__footer">
+                        <ul class="c-articleListCards__tags">
+                            <!-- テンプレート内のループ -->
+                            <?php if(isset($blogTags[$new->id])): ?>
+                                <?php foreach ($blogTags[$new->id] as $index => $blogTag) : ?>
+                                <li>
+                                    <a href="/blogs/<?= $blogTag["tag_slug"]; ?>/">
+                                        <?= $blogTag["tag_name"]; ?>
+                                    </a>
+                                </li>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                    </footer>
+                </article>
+            <?php endforeach; ?>
+          </div>
+    </div>
+
+    <div class="l-container c-viewMore">
+        <a class=" c-viewMore__link" href="/blogs">ブログをもっと見る</a>
+    </div>
+</section>
+
+<section class="p-blogs__blogs">
+    <div class="l-container l-container__common">
+        <div class="p-blogs__sectionTitle">
+           <h3 class="c-headingSize__2xl">LIFE</h3>
+           <span>日常など</span>
+        </div>
+
+        <div class="l-container__grid  l-container__grid-column3 p-blogs__articles">
+            <?php foreach ($news as $index => $new) : ?>
+                <article class="c-articleListCards">
+                    <img itemprop="image" src="/files/blogs/thumbnails/<?= sprintf("%010d", $new->id); ?>.webp" alt="ブログ <?= h($new->title); ?>のサムネイル" width="1200" height="630">
+                    <a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/" tabindex="-1" class="c-articleListCards__link"></a>
+                    <header class="c-articleListCards__header">
+                        <a class="c-articleListCards__categoryLink" href="/blogs/<?= h($new->blogs_category->category_name) ?>/" tabidnex="1">
+                            <div class="c-articleListCards__categoryName"><?= h($new->blogs_category->category_name) ?></div>
+                        </a>
+                        <span class="c-articleListCards__created"><time datetime="<?= $this->Time->format($new->created, 'yyyy-MM-dd\'T\'HH:mm:ssXXX'); ?>"><?= h($new->created->i18nFormat('yyyy-MM-dd')) ?></time></span>
+                    </header>
+                    <div class="c-articleListCards__titleContainer">
+                        <h4 class="c-articleListCards__title"><a href="/blogs/<?= h($new->blogs_category->slug) ?>/<?= h($new->slug) ?>/"><?= h($new->title) ?></a></h4>
+                    </div>
+                    <footer class="c-articleListCards__footer">
+                        <ul class="c-articleListCards__tags">
+                            <!-- テンプレート内のループ -->
+                            <?php if(isset($blogTags[$new->id])): ?>
+                                <?php foreach ($blogTags[$new->id] as $index => $blogTag) : ?>
+                                <li>
+                                    <a href="/blogs/<?= $blogTag["tag_slug"]; ?>/">
+                                        <?= $blogTag["tag_name"]; ?>
+                                    </a>
+                                </li>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                    </footer>
+                </article>
+            <?php endforeach; ?>
+          </div>
+    </div>
+
+    <div class="l-container c-viewMore">
+        <a class=" c-viewMore__link" href="/blogs">ブログをもっと見る</a>
+    </div>
+</section>
+
 <div class="container my-5 breadcrumbs-wrapper">
     <?= $this->Breadcrumbs->render() ?>
 </div>
