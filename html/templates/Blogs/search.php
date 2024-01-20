39ab4958b75480d8167f5b85c6a49a64380e2b13
devil-works <tkhr6022@gmail.com>
2024-01-21 02:18:20 +0900

    Add blog search

--- html/templates/Blogs/search.php
+++ html/templates/Blogs/search.php
@@ -5,6 +5,7 @@
  * @var \App\Model\Entity\Blog[]|\Cake\Collection\CollectionInterface $blogs
  */
 
+$this->Html->meta(["name" => "robots", "content" => "noindex",], null, ["block" => 'meta']);
 $this->Breadcrumbs->add(
     'Home',
     ['controller' => 'pages', 'action' => 'display'],
@@ -25,34 +26,19 @@ $this->Breadcrumbs->add(
 );
 $this->Breadcrumbs->add(
     '「' . $q . '」の検索結果',
-    ['controller' => 'blogs', 'action' => 'search'],
-    [
-        'templateVars' => [
-            'num' => '3',
-            'active' => 'active'
-        ]
-    ]
+    false
 );
 
 $this->Breadcrumbs->setTemplates([
-    'wrapper' => '<nav aria-label="breadcrumb"{{attrs}} ><ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">{{content}}</ol></nav>',
-    'item' => '<li class="breadcrumb-item active"{{attrs}}  itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+    'wrapper' => '<nav class="c-breadcrumb l-container l-container__common" aria-label="breadcrumb"{{attrs}} ><ol class="c-breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">{{content}}</ol></nav>',
+    'item' => '<li class="c-breadcrumb__item"{{attrs}}  itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
                 <a itemprop="item" href="{{url}}"{{innerAttrs}} class="{{active}}"><span itemprop="name">{{title}}</span></a><meta itemprop="position" content="{{num}}" />
                </li>',
-    'itemWithoutLink' => '<li class="breadcrumb-item"{{attrs}}>{{title}}</li>',
+    'itemWithoutLink' => '<li class="c-breadcrumb__item active"{{attrs}}>{{title}}</li>',
 ]);
 
 $this->assign('title', 'devil-code(デビルコード) - ブログ - 「'. $q . '」の検索結果');
 
-if(!$blog_count){
-    echo $this->Html->meta(["name"=>"robots","content"=>"noindex"],null,["block"=>'meta']);
-}else{
-        // 構造化データ
-    $structuredData = array(
-        "@context" => "https://schema.org",
-        "@type" => "ItemList"
-    );  
-}
 ?>
 
 <?= $this->Html->meta(["name"=>"description","content"=>"devil code(デビルコード)ではプログラミングの技術メモや学習記録、ITのキャリアプランや就職などについてブログを投稿しています。"],null,["block"=>'meta']); ?>
@@ -64,57 +50,45 @@ if(!$blog_count){
 <?= $this->Html->meta(["property"=>"og:site_name","content"=>"devil code"],null,["block"=>'meta']); ?>
 <?= $this->Html->meta(["property"=>"og:locale","content"=>"ja_JP"],null,["block"=>'meta']); ?>
 
-<section class="blog" style="min-height: 60vh;">
-    <div class="container">
-        <div class="row my-5 d-flex">
-            <div class="col-md-8 me-md-1">
-                <?php if ($blog_count) :?>
-                    <h2 class="mb-5">「<?= h($q) ?>」の検索結果 <?= h($blog_count) ?>件</h2>
-                    <?php foreach ($blogs as $index => $blog) : ?>
-                        <?php
-                            // 動的に構造化データを生成
-                            $index++;
-                            $itemListElement[] = array(
-                                "@type" => "ListItem",
-                                "position" => $index,
-                                "url" => BASE_URL . '/blogs/' . $blog->blogs_category->category_label . '/' . $blog->slug . '/',
-                                "name"=> $blog->title
-                            );
-                        ?>
-                        <a href="/blogs/<?= h($blog->blogs_category->category_label) ?>/<?= h($blog->slug) ?>/" class="pe-lg-5 h-100">
-                            <div class="d-md-flex justify-content-between align-items-center">
-                                <h3 class="fs-5 fs-lg-4 blog-ttl lh-base"><?= h($blog->title) ?></h3>
-                                <span class="catTag" style="background-color:<?= h($blog->blogs_category->category_color) ?>;"><?= h($blog->blogs_category->category_label) ?></span>
-                            </div>
-                            <div class="mb-4 text-muted"><span style="font-size: 12px;"><?= h($blog->created->i18nFormat('yyyy.MM.dd')) ?></div>
-                            <p class="mb-5 text-muted text-break" style="font-weight:100;"><?= mb_substr(strip_tags($blog->body),0,120) ?></p>
-                        </a>
-                    <?php endforeach; ?>
-                    <?php
-                        // 動的に生成した構造化データを格納
-                        $structuredData["itemListElement"] = $itemListElement;
-                    ?>
-                <?php else: ?>
-                    <h2 class="mb-5">「<?= h($q) ?>」の検索結果 <?= h($blog_count) ?>件</h2>
-                    <div class="d-flex justify-content-between align-items-center">
-                        「<?= h($q) ?>」に一致する記事は見つかりませんでした。
-                    </div>
-                <?php endif;?>
-            </div>
-            <div class="mt-5 col-md-3 h-100 px-md-5 sticky-top category_tagWrapper">
-                <h5>カテゴリー</h5>
-                <span>Category</span>
-                <ul class="mt-3 mt-3 p-0 categories">
-                    <?php foreach ($categories as $category) : ?>
-                        <li class="categoryLink d-inline-block">
-                            <a class="d-inline-block" href="../../<?= h($category->cat_label) ?>/"><?= h($category->cat_label) ?></a>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
+<section class="p-blogsSearch">
+    <div class="l-container l-container__common">
+        <div class="p-blogs__sectionTitle">
+        <h3 class="c-headingSize__2xl">Caterogy</h3>
+           <span>カテゴリー別</span>
         </div>
+        <div class="l-container__grid  l-container__grid-column3 p-blogs__articles">
+            <?php foreach ($posts['blogs'] as $index => $post) : ?>
+                <article class="c-articleListCards">
+                    <img itemprop="image" src="/files/blogs/thumbnails/<?= sprintf("%010d", $post->id); ?>.webp" alt="ブログ <?= h($post->title); ?>のサムネイル" width="1200" height="630">
+                    <a href="/blogs/<?= h($post->blogs_category->slug) ?>/<?= h($post->slug) ?>/" tabindex="-1" class="c-articleListCards__link"></a>
+                    <header class="c-articleListCards__header">
+                        <a class="c-articleListCards__categoryLink" href="/blogs/<?= h($post->blogs_category->category_name) ?>/" tabidnex="1">
+                            <div class="c-articleListCards__categoryName"><?= h($post->blogs_category->category_name) ?></div>
+                        </a>
+                        <span class="c-articleListCards__created"><time datetime="<?= $this->Time->format($post->created, 'yyyy-MM-dd\'T\'HH:mm:ssXXX'); ?>"><?= h($post->created->i18nFormat('yyyy-MM-dd')) ?></time></span>
+                    </header>
+                    <div class="c-articleListCards__titleContainer">
+                        <h4 class="c-articleListCards__title"><a href="/blogs/<?= h($post->blogs_category->slug) ?>/<?= h($post->slug) ?>/"><?= h($post->title) ?></a></h4>
+                    </div>
+                    <footer class="c-articleListCards__footer">
+                        <ul class="c-articleListCards__tags">
+                            <!-- テンプレート内のループ -->
+                            <?php if(isset($posts['blogTags'][$post->id])): ?>
+                                <?php foreach ($posts['blogTags'][$post->id] as $index => $blogTag) : ?>
+                                <li>
+                                    <a href="/blogs/<?= $blogTag["tag_slug"]; ?>/">
+                                        <?= $blogTag["tag_name"]; ?>
+                                    </a>
+                                </li>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                    </footer>
+                </article>
+            <?php endforeach; ?>
+          </div>
     </div>
+
 </section>
-<div class="container my-5 breadcrumbs-wrapper">
-    <?= $this->Breadcrumbs->render() ?>
-</div>
+
+<?= $this->Breadcrumbs->render() ?>
